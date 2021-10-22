284b89bca4eafb834d655b489b854a0f5811ada0
sirikorn <sirikornborisuth@gmail.com>
2021-10-22 21:33:21 +0700

    navbar

---
+++ navbar.php
@@ -0,0 +1,52 @@
+<!-- Fixed-Top -->
+<nav class="navbar navbar-expand-sm navbar-dark background-color:rgba(0,0,0,0) fix-top">
+    <!-- Content -->
+    <div class="container-fluid">
+        <!-- Brand:logo -->
+        <a href="index.php" class="navbar-brand"><img src="logotellvar.png" alt="LOGO" style="width: 130px;height: 65px;"></a>
+        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarToggle">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Navbar : Menu -->
+            <ul class="nav nav-tabs" role="tablist">
+                <li class="nav-item"><a class="navbar nav-link active" data-bs-toggle="tab" href="index.php" class="nav-link active">HOME</a></li>
+                <li class="nav-item"><a class="navbar nav-link" data-bs-toggle="tab" href="index.php" class="nav-link active">BLOG</a></li>
+                <li class="nav-item"><a class="navbar nav-link" data-bs-toggle="tab" href="index.php" class="nav-link active">TRAVEL</a></li>
+                <li class="nav-item"><a class="navbar nav-link" data-bs-toggle="tab" href="index.php" class="nav-link active">EAT</a></li>
+                <li class="nav-item"><a class="navbar nav-link" data-bs-toggle="tab" href="index.php" class="nav-link active">HOSTEL</a></li>
+            </ul>
+
+        <ul class="navbar-nav">
+            <li class=" nav-item"> <button class="button2" onclick="document.getElementById('id01').style.display='block'">
+                <span><i class='fas fa-sign-in-alt'></i></i></span> Login</button></li>
+        </ul>
+
+        <!-- Dropdown : Language -->
+        <!--ul class="nav-item dropdown">
+            <a  href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="bi bi-globe2 background-colro:#F7F6EE"></i></a>
+                <div class="dropdown-menu">
+                    <li><a href="#" class="dropdown-item">ภาษาไทย </a></li>
+                    <li><a href="#" class="dropdown-item">English(US) </a></li>
+                </div>
+        </ul-->
+
+        <!-- Dropdown : ยังไม่ได้เข้าสู่ระบบ -->
+        <!--ul class="nav-item dropdown">
+            <a  href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="bi bi-people-fill"></i></a>
+                <div class="dropdown-menu">
+                    <li><a href="#" class="dropdown-item">LOGIN </a></li>
+                    <li><a href="#" class="dropdown-item">SING UP </a></li>
+                </div>
+        </ul -->
+        <!-- Dropdown : เข้าสู่ระบบแล้ว -->
+        <!--ul class="nav-item dropdown">
+            <a  href="#" class="nav-link dropdown-toggle style-link" data-bs-toggle="dropdown"><i class="bi bi-people-fill"></i></a>
+                <div class="dropdown-menu">
+                    <li><a href="#" class="dropdown-item">PROFILE </a></li>
+                    <li><a href="#" class="dropdown-item">LISTS </a></li>
+                    <li><a href="#" class="dropdown-item">LOGOUT </a></li>
+                </div>
+        </ul-->
+
+    </div>
+</nav>
